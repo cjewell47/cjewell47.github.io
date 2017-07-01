2660615f0eeb1a05a44c888f0efc858e6f7de0ab
cjewell47 <charles.jewell47@gmail.com>
2017-07-01 14:01:11 +0100

    header animation on load

--- js/custom.js
+++ js/custom.js
@@ -1,40 +1,50 @@
-// Closes the sidebar menu
-$("#menu-close").click(function(e) {
-  e.preventDefault();
-  $("#sidebar-wrapper").toggleClass("active");
-});
+$(() => {
 
-// Opens the sidebar menu
-$("#menu-toggle").click(function(e) {
-  e.preventDefault();
-  $("#sidebar-wrapper").toggleClass("active");
-});
 
-// Scrolls to the selected menu item on the page
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
 
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
+  // Closes the sidebar menu
+  $("#menu-close").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+  });
+
+  // Opens the sidebar menu
+  $("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+  });
+
+  // Scrolls to the selected menu item on the page
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
       }
-    }
-  });
-});
-
-// Map scrolling behaviour
-$(document).ready(function() {
-  $('#map_iframe').addClass('scrolloff');
-  $('#map').on('click', function () {
-    $('#map_iframe').removeClass('scrolloff');
+    });
   });
 
-  $('#map_iframe').mouseleave(function  () {
+  // Map scrolling behaviour
+  $(document).ready(function() {
     $('#map_iframe').addClass('scrolloff');
+    $('#map').on('click', function () {
+      $('#map_iframe').removeClass('scrolloff');
+    });
+
+    $('#map_iframe').mouseleave(function  () {
+      $('#map_iframe').addClass('scrolloff');
+    });
   });
+
+  $(document).ready(function() {
+    $(this).find('.headerText, #findOut').delay(600).fadeIn(1800);
+  });
+
 });
