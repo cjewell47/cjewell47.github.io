586898f274aa696189cf9e9d474b957176f79758
cjewell47 <charles.jewell47@gmail.com>
2018-09-22 19:53:15 +0100

    set up basic template

--- js/new.js
+++ js/new.js
@@ -1,7 +1,13 @@
-// $(() => {
-//
-//   $('.header-item').hover(() => {
-//
-//   });
-//
-// });
+$(() => {
+
+  $('.header-item').click(function() {
+    $('.header-item.active').removeClass('active');
+    $(this).addClass('active');
+    var clicked = $(this).attr('data-val');
+    if (!$('.content.active').hasClass(clicked)) {
+      $('.content.active').removeClass('active');
+      $('.content.' + clicked).addClass('active');
+    }
+  });
+
+});
