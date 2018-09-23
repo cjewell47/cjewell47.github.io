1f64e3eeb4732d31ea970cd142e185f0790ddfcd
cjewell47 <charles.jewell47@gmail.com>
2018-09-23 10:26:34 +0100

    added home screen, maybe needs animations and transitions

--- js/new.js
+++ js/new.js
@@ -10,4 +10,12 @@ $(function() {
     }
   });
 
+  $('.content.default .header-item').click(function() {
+    $('.header-item.hidden').removeClass('hidden');
+    var clicked1 = $(this).attr('data-val');
+    $('.header-item.' + clicked1).addClass('current');
+    console.log($('.header-item.' + clicked1));
+
+  });
+
 });
